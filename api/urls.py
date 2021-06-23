fd37a7e60431236446d200fac1560303b6bf3b6c
Saup21 <sauparna.gupta@gmail.com>
2021-06-23 19:43:11 +0530

    Delete added and status codes added

--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('task-detail/<int:pk>/', views.task_detail, name='task_detail'),
     path('task-create/', views.task_create, name='task_create'),
     path('task-edit/<int:pk>/', views.task_edit, name='task_edit'),
+    path('task-delete/<int:pk>/', views.task_delete, name='task_delete'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import TaskSerializer
 from .models import Task
@@ -26,7 +27,10 @@ def task_list(request):
 
 @api_view(['GET'])
 def task_detail(request, pk):
-    task = Task.objects.get(pk=pk)
+    try:
+        task = Task.objects.get(pk=pk)
+    except Task.DoesNotExist:
+        return Response('Task does not exist', status=status.HTTP_404_NOT_FOUND)
     serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
 
@@ -35,10 +39,8 @@ def task_create(request):
     serializer = TaskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
-    # Need to learn how to handle errors
-    # return Response(status=status.HTTP_417_EXPECTATION_FAILED)
-    # HTTP_417_EXPECTATION_FAILED
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def task_edit(request, pk):
@@ -46,4 +48,11 @@ def task_edit(request, pk):
     serializer = TaskSerializer(instance=task, data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['DELETE'])
+def task_delete(request, pk):
+    task = Task.objects.get(pk=pk)
+    task.delete()
+    return Response('Task deleted', status=status.HTTP_200_OK)
